2f4849d0b3e5752bf9c1fc165def4a7c5a99b815
swarnim-deepsource <swarnim@deepsource.io>
2022-02-11 12:15:20 +0530

    init commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn foo(v : i32) {
+    println!("foo : {}", v);
+}
+
+fn main() {
+    let vev = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let some = &vev[0..];
+    some.iter()
+    .inspect(|test| { println!("{}", test); })
+    .for_each(|x| foo(*x)); // performing copy because pointers/refs are likely just as big as an i32
+    println!("Hello, world!");
+}
