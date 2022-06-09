b13f992b96b17291825bec5f2e6f15e747c1e75d
swarnim-deepsource <99165284+swarnim-deepsource@users.noreply.github.com>
2022-06-10 04:02:45 +0530

    update for rerun

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ fn main() {
     let vev = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let some = &vev[0..];
     some.iter()
-    .inspect(|test| { println!("{}", test); })
-    .for_each(|x| { foo(*x) }); // performing copy because pointers/refs are likely just as big as an i32
+        .inspect(|test| { println!("{}", test); })
+        .for_each(|x| { foo(*x) }); // performing copy because pointers/refs are likely just as big as an i32
     some.iter().find(|c| if **c > 8 { true } else { false });
+
     some.iter().nth(1);
+
     "oh my chars".bytes().count();
     println!("Hello, world!");
 }
