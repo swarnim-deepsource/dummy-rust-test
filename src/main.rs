3cc466e34eda836c92d45e85e80e046d5772cc57
Swarnim Arun <swarnim@deepsource.io>
2023-02-13 15:07:25 +0530

    new change

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,10 @@
-use serialport;
-
-fn foo(v : i32) {
-    println!("foo : {}", v);
-}
-
 fn main() {
-    let vev = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let some = &vev[0..];
-    some.iter()
-        .inspect(|test| { println!("{}", test); })
-        .for_each(|x| { foo(*x) }); // performing copy because pointers/refs are likely just as big as an i32
-    some.iter().find(|c| if **c > 8 { true } else { false });
-
-    some.iter().nth(1);
-
-    "oh my chars".bytes().count();
-    println!("Hello, world!");
+    if false {
+        // this is untouched comment
+    } print!("main");
 }
 
-fn test(val: i32) -> i32 {
-    let port = Port();
-    drop(Port);
-    if let Some(val) = val.checked_add(val) {
-        val
-    } else {
-        val
-    }
+#[test]
+fn test() {
+    main();
 }
